a5b506c4d33b2b6b4d250e0d350ce4f5d11b4cb5
AkbarKheun <akbarmajenesmk5@gmail.com>
2023-03-09 22:38:53 +0700

    add button

--- resources/views/backend/partial/sidebar.blade.php
+++ resources/views/backend/partial/sidebar.blade.php
@@ -5,7 +5,7 @@
         <div class="sidebar-brand-icon rotate-n-15">
             <i class="fas fa-laugh-wink"></i>
         </div>
-        <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+        <div class="sidebar-brand-text mx-3">B-Konseling</div>
     </a>
 
     <!-- Divider -->
@@ -36,9 +36,27 @@
         <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
                 <a class="collapse-item" href="{{ route('siswa-index') }}">Siswa</a>
-                <a class="collapse-item" href="{{ route('guru-create') }}">Guru</a>
+                <a class="collapse-item" href="{{ route('guru-index') }}">Guru</a>
                 <a class="collapse-item" href="{{ route('kelas-index') }}">Kelas</a>
                 <a class="collapse-item" href="cards.html">Peraturan</a>
+            </div>
+        </div>
+    </li>
+
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
+            aria-expanded="true" aria-controls="collapseUtilities">
+            <i class="fas fa-fw fa-wrench"></i>
+            <span>Utilities</span>
+        </a>
+        <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
+            data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Custom Utilities:</h6>
+                <a class="collapse-item" href="utilities-color.html">Colors</a>
+                <a class="collapse-item" href="utilities-border.html">Borders</a>
+                <a class="collapse-item" href="utilities-animation.html">Animations</a>
+                <a class="collapse-item" href="utilities-other.html">Other</a>
             </div>
         </div>
     </li>
